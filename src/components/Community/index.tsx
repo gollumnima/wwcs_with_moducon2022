d9d1863f844fb9b737599e27c92f689e4d7ea396
gollumnima <doori.alice.kim@gmail.com>
2022-11-17 01:18:49 +0900

    [wip] firestore 세팅 및 input field 추가

--- src/components/Community/index.tsx
+++ src/components/Community/index.tsx
@@ -1,7 +1,48 @@
-export const Community = () => (
-  <div>
-    <span>커뮤니티란 무엇이라고 생각하나요? 샤빱두비두밥~</span>
-    <span>질문 다른걸로 바꿔도 됩니당 아직 기획이 안되서 일단은 이렇게..!</span>
-    <span>여기서 인풋값 받아서 서버로 보낼거에용 </span>
-  </div>
-);
+import { ChangeEvent, useState } from 'react';
+import { collection, addDoc } from 'firebase/firestore';
+import { db } from '~/firebase';
+// TODO: 경로문제 해결하고, DB에 값 제대로 들어오나 확인하기
+
+export const Community = () => {
+  const [value, setValue] = useState('');
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  const onSubmit = async () => {
+    try {
+      const moduconDB = process.env.REACT_APP_FIRESTORE_DB_NAME ?? '';
+      await addDoc(collection(db, moduconDB), {
+        word: value,
+      });
+      return undefined;
+    } catch (err) {
+      return (err as Error).message;
+    }
+  };
+
+  return (
+    <div className="flex flex-col">
+      <span className="label-text text-lg my-2 mx-2">여러분이 생각하는 커뮤니티란 무엇인가요?</span>
+      <label className="label" htmlFor="community_value">
+        <input
+          type="text"
+          id="community_value"
+          className="input input-bordered input-secondery w-full"
+          onChange={onChange}
+          value={value}
+          placeholder="커뮤니티란?"
+        />
+      </label>
+      <button
+        type="button"
+        className="btn btn-primary mt-6"
+        onClick={onSubmit}
+      >
+        <span className="text-white">
+          전송
+        </span>
+      </button>
+    </div>
+  );
+};
