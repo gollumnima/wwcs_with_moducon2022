80b232b373998e2bc13ddf9c604299ed21b5018b
gollumnima <doori.alice.kim@gmail.com>
2022-11-25 01:13:39 +0900

    [add] Input 컴포넌트에 error props 추가

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -6,18 +6,19 @@ type InputProps = {
   value: string;
   placeholder: string;
   name?: string;
+  error?: boolean;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
 };
 
 export const Input = ({
-  label, value, placeholder, onChange, name,
+  label, value, placeholder, onChange, name, error,
 }:InputProps) => (
   <label className="label" htmlFor={label}>
     <input
       type="text"
       id={label}
       name={name}
-      className="input input-bordered border-solid input-primary w-full"
+      className={`input input-bordered border-solid input-primary w-full ${error && 'input-error'}`}
       onChange={onChange}
       value={value}
       placeholder={placeholder}
