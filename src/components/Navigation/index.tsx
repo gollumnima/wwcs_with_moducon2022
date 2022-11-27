c7782d8f901398ab2d9355bb5abacefbf439539b
gollumnima <doori.alice.kim@gmail.com>
2022-11-28 01:01:21 +0900

    [modify] 네비게이션에 동적 컬러 클래스명 추가 및 링크와 이모티콘 추가

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,28 +1,36 @@
-export const Navigation = () => (
-  <div className="navbar mb-2 shadow-lg bg-neutral text-neutral-content">
-    <div className="flex-none px-2 mx-2">
-      <span className="text-lg font-bold">
-        WWCS X MODUCON
-      </span>
-    </div>
-    <div className="flex-1 px-2 mx-2">
-      <div className="items-stretch lg:flex">
-        <a href="/community">
-          <span className="btn btn-ghost btn-sm rounded-btn">
-            커뮤니티란?
+import { Link } from 'react-router-dom';
+import { usePathname } from 'src/utils/usePathname';
+
+export const Navigation = () => {
+  const { mainColor } = usePathname();
+  return (
+    <div className={`navbar mb-2 shadow-lg bg-${mainColor} text-${mainColor}-content`}>
+      <div className="flex-none px-2 mx-2">
+        <Link to="/">
+          <span className="text-lg font-bold">
+            WWCS X MODUCON
           </span>
-        </a>
-        <a href="/footprint">
-          <span className="btn btn-ghost btn-sm rounded-btn">
-            발도장
-          </span>
-        </a>
-        <a href="/result">
-          <span className="btn btn-ghost btn-sm rounded-btn">
-            구름
-          </span>
-        </a>
+        </Link>
+      </div>
+      <div className="flex-1 px-2 mx-2">
+        <div className="items-stretch lg:flex">
+          <a href="/community">
+            <span className="btn btn-ghost btn-sm rounded-btn">
+              😎 커뮤니티란?
+            </span>
+          </a>
+          <a href="/footprint">
+            <span className="btn btn-ghost btn-sm rounded-btn">
+              🐾 발도장
+            </span>
+          </a>
+          <a href="/result">
+            <span className="btn btn-ghost btn-sm rounded-btn">
+              ☁️ 구름
+            </span>
+          </a>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
