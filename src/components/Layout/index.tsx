372b35458b988bf354ca95eb23ac91fcb39145e0
gollumnima <doori.alice.kim@gmail.com>
2022-12-08 01:50:38 +0900

    [modify] pathname 변경될때마다 이전 페이지의 스크롤 위치 초기화

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,5 +1,6 @@
-import { PropsWithChildren } from 'react';
-import { Outlet } from 'react-router-dom';
+/* eslint-disable no-undef */
+import { PropsWithChildren, useLayoutEffect } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import { Footer } from '../Footer';
 import { Navigation } from '../Navigation';
 
@@ -7,13 +8,22 @@ type Props = {
 
 };
 
-export const Layout = ({ children }: PropsWithChildren<Props>) => (
-  <div className="relative h-full sm:h-screen">
-    <Navigation />
-    <div className="px-2 max-w-2xl mx-auto pt-28 pb-80 sm:pb-64">
-      {children}
-      <Outlet />
+export const Layout = ({ children }: PropsWithChildren<Props>) => {
+  const { pathname } = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }, [pathname]);
+  return (
+    <div className="relative h-full sm:h-screen">
+      <Navigation />
+      <div className="px-2 max-w-2xl mx-auto pt-28 pb-80 sm:pb-64">
+        {children}
+        <Outlet />
+      </div>
+      <Footer />
     </div>
-    <Footer />
-  </div>
-);
+  );
+};
