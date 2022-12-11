3f77c3d958e799903feff418940e26717e0756c4
gollumnima <doori.alice.kim@gmail.com>
2022-12-11 16:54:57 +0900

    [modify] twitter meta tag 수정 22

--- src/components/Meta.tsx
+++ src/components/Meta.tsx
@@ -8,20 +8,20 @@ interface MetaProps {
 export const Meta = ({ title } :PropsWithChildren<MetaProps>) => (
   <Helmet>
     <title>{title}</title>
-    <meta name="description" content="모두콘 2022에서 위민후코드 서울 커뮤니티 세션 발표용 웹사이트 입니다. 커뮤니티를 한 단어로 나타내고, 각자 어떤 생각을 가지고 있는지 키워드를 통해 확인해볼 수 있습니다." />
+    <meta name="description" content="모두콘 2022에서 위민후코드 서울 커뮤니티 세션 발표용 웹사이트 입니다. 커뮤니티를 한 단어로 나타내고, 각자 어떤 생각을 가지고 있는지 키워드를 통해 확인해볼 수 있습니다. #WomenWhoCodeSeoul #위민후코드서울" />
     <meta property="og:title" content="위민후코드 서울 X 모두콘 2022" />
     <meta property="og:url" content="https://moducon2022-5fa7d.web.app/" />
     <meta property="og:type" content="website" />
-    <meta property="og:image" content="/images/og-image.png" />
+    <meta property="og:image" content="https://raw.githubusercontent.com/gollumnima/wwcs_with_moducon2022/main/public/images/og-image.png" />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="627" />
-    <meta property="og:description" content="모두콘 2022에서 위민후코드 서울 커뮤니티 세션 발표용 웹사이트 입니다. 커뮤니티를 한 단어로 나타내고, 각자 어떤 생각을 가지고 있는지 키워드를 통해 확인해볼 수 있습니다." />
+    <meta property="og:description" content="모두콘 2022에서 위민후코드 서울 커뮤니티 세션 발표용 웹사이트 입니다. 커뮤니티를 한 단어로 나타내고, 각자 어떤 생각을 가지고 있는지 키워드를 통해 확인해볼 수 있습니다. #WomenWhoCodeSeoul #위민후코드서울" />
 
     <meta name="twitter:creator" content="@WWCodeSeoul" />
     <meta name="twitter:site" content="@WWCodeSeoul" />
     <meta name="twitter:url" content="https://moducon2022-5fa7d.web.app/" />
-    <meta name="twitter:card" content="/images/og-image.png" />
-    <meta name="twitter:title" content="위민후코드 서울 X 모두콘 2022" />
-    <meta name="twitter:image" content="/images/og-image.png" />
+    <meta name="twitter:card" content="모두콘 2022에서 위민후코드 서울 커뮤니티 세션 발표용 웹사이트 입니다. 커뮤니티를 한 단어로 나타내고, 각자 어떤 생각을 가지고 있는지 키워드를 통해 확인해볼 수 있습니다. #WomenWhoCodeSeoul #위민후코드서울" />
+    <meta name="twitter:title" content="커뮤니티에 대한 키워드를 정의해보고 워드 클라우드로 확인해보세요! by.위민후코드 서울" />
+    <meta name="twitter:image" content="https://raw.githubusercontent.com/gollumnima/wwcs_with_moducon2022/main/public/images/og-image.png" />
   </Helmet>
 );
