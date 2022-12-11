8b0e1eed00d7ef38ef5c2e2295434f17505f94db
gollumnima <doori.alice.kim@gmail.com>
2022-12-11 17:28:26 +0900

    [modify] helmet 삭제

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
 /* eslint-disable no-undef */
 import ReactDOM from 'react-dom/client';
-import { HelmetProvider } from 'react-helmet-async';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './Routes';
 import './styles/main.css';
 import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <HelmetProvider>
-    <RouterProvider router={router} />
-  </HelmetProvider>,
+  <RouterProvider router={router} />,
 );
